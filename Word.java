9350956a54421f15537418c05360a121b6c50233
Jardel Anton <jardellanton@gmail.com>
2015-03-30 19:17:45 -0300

    Classe para armazenar strings

---
+++ Word.java
@@ -0,0 +1,21 @@
+class Word{
+	private String name;
+	private String value;
+
+
+	public setCaracters_name(String name){
+		this.name=name;
+	}
+
+	public setCaracters_value(String value){
+		this.value = value;
+	}
+
+	public getCaracters_name(){
+		return this.name;
+	}
+	
+	public getCaracters_value(){
+		return this.value;
+	}
+}
