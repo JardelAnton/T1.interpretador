af9ca082825025ae37bcf3b28848d4df10f7105f
Jardel Anton <jardellanton@gmail.com>
2015-03-30 16:11:18 -0300

    Variavel do tipo numero criação/getters/setters

---
+++ Number.java
@@ -0,0 +1,21 @@
+class Number{
+	private String name;
+	private Double value;
+
+
+	public setNumber_name(String name){
+		this.name=name;
+	}
+
+	public setNumber_value(Double Value){
+		this.value = value;
+	}
+
+	public getNumber_name(){
+		return this.name;
+	}
+	
+	public getNumber_value(){
+		return this.value;
+	}
+}
